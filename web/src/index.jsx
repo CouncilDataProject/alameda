fb9ed969bf63eccb7c6d93ec572d793576d439dd
CouncilDataProjectServiceAccount <councildataproject@gmail.com>
2022-05-27 22:11:11 +0000

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-alameda-d3dabe54",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "The City of Alameda",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
