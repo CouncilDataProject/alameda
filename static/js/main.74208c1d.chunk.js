ade33b40c743af0e17f8ece2b5e74cffd1688977
CouncilDataProjectServiceAccount <CouncilDataProjectServiceAccount@users.noreply.github.com>
2022-10-03 23:54:50 +0000

    Deploying to gh-pages from @ CouncilDataProject/alameda@395806cde3d7be68bc27b01a4d107c83d2b20c12 🚀

--- static/js/main.74208c1d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcdp-alameda"]=this["webpackJsonpcdp-alameda"]||[]).push([[0],{201:function(e,i){},88:function(e,i,n){"use strict";n.r(i);n(0);var t=n(25),a=n.n(t),o=n(63),c=(n(215),n(54));a.a.render(Object(c.jsx)("div",{children:Object(c.jsx)(o.AppConfigProvider,{appConfig:{firebaseConfig:{options:{projectId:"cdp-alameda-d3dabe54"},settings:{}},municipality:{name:"The City of Alameda",timeZone:"America/Los_Angeles",footerLinksSections:[]}},children:Object(c.jsx)(o.App,{})})}),document.getElementById("root"))}},[[88,1,2]]]);
-//# sourceMappingURL=main.74208c1d.chunk.js.map
